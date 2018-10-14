c3ea96ba4d496eb12054bee897f41ce89f584f31
afeiship <1290657123@qq.com>
2018-10-14 20:38:17 +0800

    issue: refactor

--- dist/next-wx-option-menu.js
+++ dist/next-wx-option-menu.js
@@ -1,30 +1,37 @@
-(function () {
+(function() {
+  var global = global || this || window || Function("return this")();
+  var nx = global.nx || require("next-js-core2");
+  var wx = (global.wx = global.wx);
 
-  var global = global || this || self || window;
-  var nx = global.nx || require('next-js-core2');
-  var wx = global.wx = global.wx;
-
-  var NxWxOptionMenu = nx.declare('nx.WxOptionMenu', {
+  var NxWxOptionMenu = nx.declare("nx.WxOptionMenu", {
     statics: {
-      show: function(){
-        wx.ready(function(){
+      show: function() {
+        if (global.__WEIXIN_READY___) {
           wx.showOptionMenu();
-        });
+        } else {
+          reject({
+            status: "fail",
+            msg: "微信SDK还未初始化"
+          });
+        }
       },
-      hide: function(){
-        wx.ready(function(){
+      hide: function() {
+        if (global.__WEIXIN_READY___) {
           wx.hideOptionMenu();
-        });
+        } else {
+          reject({
+            status: "fail",
+            msg: "微信SDK还未初始化"
+          });
+        }
       },
-      visible: function(inValue){
-        return inValue ? this.show(): this.hide();
+      visible: function(inValue) {
+        return inValue ? this.show() : this.hide();
       }
     }
   });
 
-
-  if (typeof module !== 'undefined' && module.exports) {
+  if (typeof module !== "undefined" && module.exports) {
     module.exports = NxWxOptionMenu;
   }
-
-}());
+})();
